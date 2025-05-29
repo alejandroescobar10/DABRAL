6b0ddf9a7fe5a4284896e422eb206b3d05427f16
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-05-28 22:51:43 -0500

    backend

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import "reflect-metadata";
 import express from "express";
 import { AppDataSource } from "./data-source";
 import bookRoutes from './routes/bookRoutes';
+import cors from "cors";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use('/books', bookRoutes);
 
@@ -13,8 +15,8 @@ AppDataSource.initialize()
     console.log("Conexión a la base de datos exitosa");
 
     // Iniciar el servidor
-    app.listen(3000, () => {
-      console.log("Servidor corriendo en http://localhost:3000");
+    app.listen(5000, () => {
+      console.log("Servidor corriendo en http://localhost:5000");
     });
   })
   .catch((error) => console.error("Error al conectar a la base de datos:", error));
