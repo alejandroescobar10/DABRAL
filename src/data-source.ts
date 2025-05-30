fc85da728d36689c4190dc30346bb13e449eb77b
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-05-29 23:17:02 -0500

    fronten 6

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,16 @@
-import "reflect-metadata";
 import { DataSource } from "typeorm";
-import { Libro } from "./entity/book"; // Asegúrate de que la ruta sea correcta
+import { Libro } from "./entity/book";
 
 export const AppDataSource = new DataSource({
-  synchronize: true, // Sincroniza automáticamente las entidades con la base de datos (solo para desarrollo)
-  logging: false, // Cambia a true si necesitas ver las consultas SQL en la consola
-  type: "sqlite", // Tipo de base de datos
-  database: "database.sqlite", // Nombre del archivo de la base de datos
-  entities: [Libro], // Lista de entidades
-  subscribers: [], // Lista de suscriptores (si los usas)
-  migrations: [], // Lista de migraciones (si las usas)
+  type: "postgres",
+  host: "nwezqzsvkzauovpmlsyv.supabase.co",           // ej: db.xxxxxx.supabase.co
+  port: 5432,
+  username: "postgres",    // usualmente: postgres
+  password: "zaq12345",
+  database: "backendLibros",   // el nombre del proyecto Supabase
+  synchronize: true,
+  logging: false,
+  entities: [Libro],
+  migrations: [],
+  subscribers: [],
 });
