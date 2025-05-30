4a2758ab50fed61ba7d9e51443eb8d81639e78fd
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-05-29 23:20:30 -0500

    fronten 6

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ import bookRoutes from './routes/bookRoutes';
 import cors from "cors";
 
 const app = express();
-app.use(cors())
+app.use(cors({
+  origin: 'https://frontend-libros-one.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/books', bookRoutes);
 
