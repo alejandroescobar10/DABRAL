cc4f726488ea8318adcd37116c99b18173f62f24
alejandroescobar10 <160554099+alejandroescobar10@users.noreply.github.com>
2025-06-02 22:15:29 -0500

    se actualiza back

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,9 +1,12 @@
 import { DataSource } from "typeorm";
 import { Libro } from "./entity/book";
+import path from 'path';
 const isProd = process.env.NODE_ENV === "production";
 export const AppDataSource = new DataSource({
   type: 'sqlite',
-  database: 'db.sqlite',
+  database: isProd
+    ? '/tmp/db.sqlite' // ⚠️ No recomendado: sólo lectura temporal en Vercel
+    : path.resolve(__dirname, 'db.sqlite'),
   synchronize: true,
-  entities: [Libro], // ✅ asegúrate que Libro esté aquí
+  entities: [Libro],// ✅ asegúrate que Libro esté aquí
 });
